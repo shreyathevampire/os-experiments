68ad22a6df76bf0e2acff93e3b2d1946c2891633
shreyathevampire <31374151+shreyathevampire@users.noreply.github.com>
2017-10-02 16:01:43 +0530

    Add files via upload
    implementation of paging in c
    shows the physical as well as the logical address in binary as well as decimal
    i took max size of array as 20 so if you wish to take physical space more than 1MB then do change the size of array.

---
+++ paging.c
@@ -0,0 +1,138 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+typedef struct pt
+{
+	int fno;
+	int bit;
+}pt;
+
+int contobin(int la, int arr[])
+{
+		int r,i=0,c=0;
+		while(la>0)
+		{
+				r = la%2;
+				arr[i++] = r;
+				c++;
+				la = la/2;
+	}
+			return c;
+}
+
+int contodec(int arr[], int n)
+{
+		int i=n,sum=0,a=n;
+		while(i>=0)
+		{
+				sum = sum + (arr[i--]*pow(2,a));
+					a = a-1;
+		}
+			return sum;
+}
+
+int noofbits(int la)
+{
+		int r,c=0, i=0;
+		while(la>0)
+		{
+				r= la%2;
+				c++;
+				la = la/2;
+	}
+			return c;
+}
+
+void main()
+{
+		int la,pa,f,n,i,pg,c=0,r,z,m,nla,b,d,addr,sum=0,pag,a;
+		int no,dec, arr[20], arr1[20];
+		int copy[20]={0};
+
+		struct pt pte[20];
+
+		for(i=0;i< 20 ; i++)
+				pte[i].bit=-1;
+		printf("enter logical address in kB\n");
+				scanf("%d",&la);
+		la = la * pow(10,3);
+	 			c = noofbits(la);
+		nla=c;                     //no of bits in logical address
+		printf("no of bits in logical address =%d \n", c);
+		printf("enter physical address in kB\n");
+				scanf("%d",&pa);
+				pa = pa * pow(10,3);
+				c = noofbits(pa);
+				b=c;                          //no of bits in physical address
+		printf("no of bits in physical address =%d   \n", c);
+		printf("enter size of frame in kB   \n");
+				scanf("%d",&f);
+				f = f * pow(10,3);
+				c = noofbits(f);
+				d=c;                             //no of bits in size of frame
+		printf("no of bits in f =%d \n", c);
+		printf("enter no of page table entry\n");
+				scanf("%d",&n);
+		printf("enter page table entries\n");
+		for(i=0;i<n;i++)
+		{
+				printf("enter page no and frame no\n");
+						scanf("%d",&pg);
+						scanf("%d",&pte[pg].fno);
+				pte[pg].bit=1;
+			}
+		for(i=0;i<20;i++)
+		{
+				if(pte[i].bit==1)
+						printf(" frame no [%d]=%d\n",i,pte[i].fno);
+			}
+  	printf("enter logical address\n");
+						scanf("%d",&addr);
+			i = contobin(addr, arr);
+				m=i;
+		printf("%d \n",m);
+				i=nla-1;
+		while(i>=0)
+				printf("%d ",arr[i--]);
+		printf("\n");
+		m = nla - d;
+		for( i= 0; i< m;i++)
+				copy[i] = arr[d+i];
+		for( i= 0; i< m;i++)
+					printf("%d ",copy[i]);
+		printf("\n");
+			pag = contodec(copy,m-1);
+		printf("page no = %d \n", pag);
+				i=m-1;
+		no = pte[pag].bit;
+		if(no == -1)
+		{
+				printf("frame not avaialable\n");
+				return;
+	    }
+		else
+				printf("frame no = %d \n", pte[pag].fno);
+		i=0;
+		m=pte[pag].fno;
+		while(m>0)
+		{
+					r= m%2;
+					copy[i++]= r;
+					m = m/2;
+	    }
+		for(i=(b-d)-1;i>=0 ; i--)
+				printf("%d ",copy[i]);
+		printf("\n");
+		a=0;
+		for(i=d ;i<b ;i++)
+				arr[i] = copy[a++];
+
+		i=b-1;
+		while(i>=0)
+					printf("%d ",arr[i--]);
+		printf("\n");
+		dec = contodec(arr,b-1);
+		printf("physical address = %d\n",dec);
+		printf("\n");
+}
